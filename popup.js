b6f6659b84d00dc1073cc141c0d5582d304c4a41
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-02-22 22:43:31 +0200

    Add options to capitalize words and add dashes between words

--- popup.js
+++ popup.js
@@ -12,12 +12,17 @@ function capitalize(string) {
 document.getElementById("generatePassword").addEventListener("click", function (e) {
     console.log("click")
     const numberOfWords = parseInt(document.getElementById("numberOfWords").value);
+    const capitalizeWords = document.getElementById('capitalizeWords').checked;
+    const dashesBetweenWords = document.getElementById('dashesBetweenWords').checked;
 
     chrome.storage.local.get(["words"]).then((result) => {
         const words = result.words
         let allWords = ""
         for (let i = 0; i < numberOfWords; i++) {
-            allWords += capitalize(selectWord(words))
+            let selectedWord = selectWord(words)
+            if (capitalizeWords) { selectedWord = capitalize(selectedWord) }
+            if (dashesBetweenWords && i > 0) { selectedWord = `-${selectedWord}` }
+            allWords += selectedWord
         }
         document.getElementById("generatedPassword").innerHTML = allWords
     });
