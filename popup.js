b2600e7585d86f58d9ca8e3c5f9c69f0f39e4f9d
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-02-22 22:26:33 +0200

    Add option for number of words

--- popup.js
+++ popup.js
@@ -1,18 +1,23 @@
-const NUMBER_OF_WORDS = 4
 
 function selectWord(wordList) {
     // TODO: select with cryptographically acceptable randomness
     return wordList[Math.floor(Math.random() * wordList.length)];
 }
 
+function capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 
 document.getElementById("generatePassword").addEventListener("click", function (e) {
     console.log("click")
+    const numberOfWords = parseInt(document.getElementById("numberOfWords").value);
+
     chrome.storage.local.get(["words"]).then((result) => {
         const words = result.words
         let allWords = ""
-        for (let i = 0; i < NUMBER_OF_WORDS; i++) {
-            allWords += selectWord(words)
+        for (let i = 0; i < numberOfWords; i++) {
+            allWords += capitalize(selectWord(words))
         }
         document.getElementById("generatedPassword").innerHTML = allWords
     });
