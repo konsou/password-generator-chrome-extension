2a2c31d451d29779605544143ae0a19b9b633dfe
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-02-22 11:23:38 +0200

    Select multiple words

--- popup.js
+++ popup.js
@@ -1,13 +1,20 @@
-console.log("JOU")
+const NUMBER_OF_WORDS = 4
+
+function selectWord(wordList) {
+    // TODO: select with cryptographically acceptable randomness
+    return wordList[Math.floor(Math.random() * wordList.length)];
+}
 
 
 document.getElementById("generatePassword").addEventListener("click", function (e) {
     console.log("click")
     chrome.storage.local.get(["words"]).then((result) => {
         const words = result.words
-        // TODO: select with cryptographically acceptable randomness
-        const selectedWord = words[Math.floor(Math.random() * words.length)];
-        document.getElementById("generatedPassword").value = selectedWord
+        let allWords = ""
+        for (let i = 0; i < NUMBER_OF_WORDS; i++) {
+            allWords += selectWord(words)
+        }
+        document.getElementById("generatedPassword").value = allWords
     });
 
 })
