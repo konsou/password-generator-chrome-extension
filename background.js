1c069c4a7c8c5a93ac89ef8556ffaf232a73a78f
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-02-22 11:23:49 +0200

    Cleanup

--- background.js
+++ background.js
@@ -1,16 +1,10 @@
-console.log("service worker started")
-
 chrome.runtime.onInstalled.addListener(() => {
-    console.log("onInstalled fired")
-    // TODO: do this in background worker
-
-
     fetch("words/kotus-sanalista.json")
         .then(Response => Response.json())
         .then(data => {
-            chrome.storage.local.set({"words": data}).then(() => {
-                console.log(data.length + " words loaded");
-            });
+            chrome.storage.local.set({"words": data})
+                .then(() => {
+                    console.log(data.length + " words loaded");
+                });
         });
-
 });
